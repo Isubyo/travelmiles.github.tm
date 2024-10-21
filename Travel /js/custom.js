ec95bd2e296a5f9b1f8b2096125ad820954b7480
Isubyo <innonkusi@gmail.com>
2024-10-21 17:20:26 +0200

    Add files via upload
    new files

---
+++ Travel /js/custom.js
@@ -0,0 +1,45 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(function() {
+      $('.hero-slides').vegas({
+          slides: [
+              { src: 'images/slides/photo 1.jpg' },
+              { src: 'images/photo 2.jpg' },
+              { src: 'images/photo 3.jpeg' }
+          ],
+          timer: false,
+          animation: 'kenburns',
+      });
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height() + 60;
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+  
+  })(window.jQuery);
+
+
